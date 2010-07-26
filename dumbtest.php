4a3b0ce532de342120d0002c5a4d78d5145c9f1c
Lars Warholm <law@ez.no>
2010-07-26 13:36:58 +0200

    second commit

--- dumbtest.php
+++ dumbtest.php
@@ -1,5 +1,17 @@
 <?php
 
-echo 'Hello world!'
+function fun($i) {
+    if ($i<0)
+    {
+        return;
+    }
+    else
+    {
+        echo 'Hello world!';
+        fun($i-1);
+    }
+}
+
+fun(4);
 
 ?>
